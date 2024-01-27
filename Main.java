a42bb0ded7bcb65b03d02ff6f5d84494cafe8c0e
Pankaj <chotuyadav.cy75@gmail.com>
2024-01-27 17:37:04 +0530

    aws testing

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("pankaj yadav ");
         System.out.println(new Date());
         System.out.println("one more changes");
+        System.out.println("this is the AWS testin gfor java");
     }
 }
